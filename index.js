7995c4501aafa884f300dcf551046dae1758e8a8
Sajeeh Abdussamad <sajeehabdussamad@gmail.com>
2024-07-24 17:30:50 +0300

    Major Update

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-function copyMail() {
-    navigator.clipboard.writeText("sajeehabdussamad@gmail.com");
-}
+
+// ----------------------------------------------------------------------------
+
+// Dragging
+
+// var mousePosition;
+// var offset = [0,0];
+// var vscode;
+// var isDown = false;
+
+// vscode = document.getElementById("vs");
+// vstop = document.getElementById("vstop");
+
+
+// vstop.addEventListener('mousedown', function(e) {
+//     isDown = true;
+//     offset = [
+//         vscode.offsetLeft - e.clientX,
+//         vscode.offsetTop - e.clientY
+//     ];
+// }, true);
+
+// document.addEventListener('mouseup', function() {
+//     isDown = false;
+// }, true);
+
+// document.addEventListener('mousemove', function(event) {
+//     event.preventDefault();
+//     if (isDown) {
+//         mousePosition = {
+
+//             x : event.clientX,
+//             y : event.clientY
+
+//         };
+//         vscode.style.left = (mousePosition.x + offset[0]) + 'px';
+//         vscode.style.top  = (mousePosition.y + offset[1]) + 'px';
+//     }
+// }, true);
+
+// ------------------------------------------------------------------------------
